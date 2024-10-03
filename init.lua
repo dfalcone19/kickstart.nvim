074de11097c3fc82e41ecdaf42cb08c2f4800fa2
Daniel Falcone <dfalcone19@gmail.com>
2024-10-03 18:17:06 -0400

    changed colourscheme

--- init.lua
+++ init.lua
@@ -502,6 +502,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        arduino_language_server = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
@@ -732,7 +733,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'retrobox'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
